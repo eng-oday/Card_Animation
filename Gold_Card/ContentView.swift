30004e82d42971c73ba75d62c39b994bf88a40ae
eng-oday <odaydieg@gmail.com>
2023-07-31 21:20:05 +0300

    Initial Commit

---
+++ Gold_Card/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Gold_Card
+//
+//  Created by user on 31/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Gold_Card/Gold_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gold_CardApp.swift
+//  Gold_Card
+//
+//  Created by user on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Gold_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
